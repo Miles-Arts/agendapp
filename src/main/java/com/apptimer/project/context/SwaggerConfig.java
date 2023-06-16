8f0047d10a0ff84541fa94b1723aa4790adfe61e
Milton Figueredo Miles <gratno@gmail.com>
2023-06-16 12:53:50 -0500

    Solución error SwaggerConfig

--- src/main/java/com/apptimer/project/context/SwaggerConfig.java
+++ src/main/java/com/apptimer/project/context/SwaggerConfig.java
@@ -1,20 +1,21 @@
 package com.apptimer.project.context;
 
-import io.swagger.annotations.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
-@Configuration  //Clase que crea BEANs
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
 
     @Bean
     public Docket api() {
@@ -28,11 +29,12 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
-                "App Timer API",
-                "La API REST App Timer.",
+                "Spring Boot App Timer API",
+                "Project Timer APP",
                 "v1",
-                "Terms of service",
-                new Contact("Milton F", "www.apptimer.com", "apptimercontact@apptimer.com"),
+                "Terms of Service",
+                new Contact("TIMER app Tech", "www.example.com", "myeaddress@company.com"),
                 "License of API", "API license URL", Collections.emptyList());
     }
 }
+
---
+++ src/main/java/com/apptimer/project/controller/TaskController.java
@@ -0,0 +1,11 @@
+package com.apptimer.project.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+
+
+}
